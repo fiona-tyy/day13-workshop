61aae7a760d2897c1b21a18109f75b3ec9036869
fiona-tyy <zyyfiona@hotmail.com>
2023-03-01 08:58:44 +0800

    initial commit

---
+++ src/main/java/tfip/ssf/day13workshop/Day13WorkshopApplication.java
@@ -0,0 +1,13 @@
+package tfip.ssf.day13workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day13WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day13WorkshopApplication.class, args);
+	}
+
+}
